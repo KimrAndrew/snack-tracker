3c457cc69809008637bd180647eca35334e8ef93
KimrAndrew <andrewrk1997@gmail.com>
2022-01-06 15:22:35 -0800

    added test for confirming snack page is accessible

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class snacksTest(SimpleTestCase):
+    def test_snacks_page_status_code(self):
+        url = reverse('snacks')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
