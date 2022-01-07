451ecdeda1bac9dd2f0f98b828c5c883edad8f92
KimrAndrew <andrewrk1997@gmail.com>
2022-01-06 19:05:07 -0800

    changed tests to use TestCase instead of SimpleTestCase

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,16 +1,17 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
 
 # Create your tests here.
 
-class snacksTest(SimpleTestCase):
+class snacksTest(TestCase):
     def test_snacks_page_status_code(self):
         url = reverse('snacks')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-        
+
     def test_snacks_page_template(self):
         url = reverse('snacks')
         response = self.client.get(url)
-        self.assertTemplateUsed(response, 'snacks.html')
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
