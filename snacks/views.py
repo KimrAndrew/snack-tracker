74fa9cad79c8d939333bb5b7b51dae75ee7dfe6d
KimrAndrew <andrewrk1997@gmail.com>
2022-01-06 16:14:19 -0800

    created model for Snacks starting on SnackListView

--- snacks/views.py
+++ snacks/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Snack
 
 # Create your views here.
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+
 class SnackPageView(TemplateView):
     template_name = 'snacks.html'
